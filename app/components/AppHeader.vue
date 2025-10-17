8adcee5fc9d361851c3a836cfe6951f8a94003cf
Tom <tom.van.veen@visma.com>
2025-10-17 09:44:29 +0200

    fix: update app name and description for consistency

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,52 +1,16 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
-
-interface HeaderItem {
-  label: string
-  to?: string
-  href?: string
-  external?: boolean
-}
-
-const navigationItems: HeaderItem[] = [
-  {
-    label: 'Docs',
-    href: 'https://github.com/dipodidae/jira-issue-writer#readme',
-    external: true,
-  },
-  {
-    label: 'GitHub',
-    href: 'https://github.com/dipodidae/jira-issue-writer',
-    external: true,
-  },
-]
 </script>
 
 <template>
   <UHeader>
     <template #left>
-      <NuxtLink to="/">
+      <UButton icon="fa6-solid:wand-magic" variant="link" size="xl">
         {{ appName }}
-      </NuxtLink>
+      </UButton>
     </template>
-
     <template #right>
       <div class="flex items-center gap-3">
-        <nav class="hidden items-center gap-3 md:flex" aria-label="Primary">
-          <ULink
-            v-for="item in navigationItems"
-            :key="item.label"
-            :href="item.href"
-            :to="item.to"
-            :external="item.external"
-            :target="item.external ? '_blank' : undefined"
-            :rel="item.external ? 'noopener noreferrer' : undefined"
-            class="text-muted hover:text-primary text-sm font-medium transition-colors"
-          >
-            {{ item.label }}
-          </ULink>
-        </nav>
-
         <UColorModeButton />
       </div>
     </template>
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,5 @@
-export const appName = 'Jira Ticket Creator'
-export const appDescription = 'Create Jira tickets from natural language prompts'
+export const appName = 'Jira Ticket Writer'
+export const appDescription = 'Write Jira tickets from natural language prompts'
 
 export { ISSUE_TYPE_KEYS, ISSUE_TYPES } from './issue-types'
 export { SCOPE_DESCRIPTIONS } from './scopes'
